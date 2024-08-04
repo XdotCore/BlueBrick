837784ed6df0c61ad3fbccb910f843a0055c23aa
XdotCore <xandercorvalan@gmail.com>
2024-08-04 12:54:00 -0500

    Switch to rcmp hooking library

--- loader/src/main.cpp
+++ loader/src/main.cpp
@@ -1,12 +1,15 @@
 #include <windows.h>
-#include "detours/detours.h"
 #include <format>
+#include <cstdint>
+#include <rcmp.hpp>
 
 void AttachHooks() {
-	DetourTransactionBegin();
-	DetourUpdateThread(GetCurrentThread());
+	intptr_t base = (intptr_t)GetModuleHandle(NULL);
 
-	DetourTransactionCommit();
+	rcmp::hook_function<rcmp::cdecl_t<void(uint64_t*, uint64_t, bool, bool)>>(0x7E1070 + base, [](auto original, uint64_t* coinsPtr, uint64_t toAdd, bool multEnabled, bool param_4) {
+		MessageBoxA(NULL, "Coin", "Coin", MB_OK);
+		original(coinsPtr, toAdd, multEnabled, param_4);
+	});
 }
 
 BOOL WINAPI DllMain(HINSTANCE dll, DWORD reason, LPVOID _) {
